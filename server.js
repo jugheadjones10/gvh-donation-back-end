a00f85ca9c43d6ba35d4214aad24df6861735659
Kim Young Jin <kimyoungjin1001@gmail.com>
2021-01-23 16:00:42 +0900

    G

--- server.js
+++ server.js
@@ -60,24 +60,18 @@ app.post("/donation-form", async function (req, res) {
     res.send(ID)
 })
 
-const calculateOrderAmount = items => {
-    // Replace this constant with a calculation of the order's amount
-    // Calculate the order total on the server to prevent
-    // people from directly manipulating the amount on the client
-    return 1400;
-};
 
-app.post("/create-payment-intent", async (req, res) => {
-    const { items } = req.body;
-    // Create a PaymentIntent with the order amount and currency
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: calculateOrderAmount(items),
-        currency: "usd"
-    })
-    res.send({
-        clientSecret: paymentIntent.client_secret
-    })
-})
+// app.post("/create-payment-intent", async (req, res) => {
+//     const { items } = req.body;
+//     // Create a PaymentIntent with the order amount and currency
+//     const paymentIntent = await stripe.paymentIntents.create({
+//         amount: calculateOrderAmount(items),
+//         currency: "usd"
+//     })
+//     res.send({
+//         clientSecret: paymentIntent.client_secret
+//     })
+// })
 
 app.listen(port, () =>
     console.log(`Example app listening on port ${port}!`)
