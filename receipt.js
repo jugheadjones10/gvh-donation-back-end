cfa238d7ebb31f9dd4ffabf39283613e56551f00
Kim Young Jin <kimyoungjin1001@gmail.com>
2022-07-12 08:41:01 +0000

    G

--- receipt.js
+++ receipt.js
@@ -21,34 +21,36 @@ app.use(express.json());
 
 function tallyAmounts(amount) {
   console.log("Attempting to tally amount " + amount);
-  const thePromise = redis
+  const tallyPromise = redis
     .multi()
     .hgetall(amount)
-    .lrange("donors" + amount, 0, 1)
+    .lrange("donors" + amount, 0, -1)
     .exec()
     .then((result) => {
       console.log(result);
 
-      console.log("THE RESULT", result[1][1]);
-      const promisesArray = result[1][1].map((ID) => {
-        console.log("gotten DI", ID);
+      const actionsPromises = result[1][1].map((ID) => {
         return redis.get("ID" + ID).then((userData) => {
-          console.log("the user data", userData);
+          console.log("user data", userData);
           if (result[0][1].pending === result[0][1].confirmed) {
-            sendReceipt(JSON.parse(userData));
+            return sendReceipt(JSON.parse(userData));
           } else {
-            requestManualCheck(JSON.parse(userData));
+            return requestManualCheck(JSON.parse(userData));
           }
         });
       });
-      promisesArray.push(redis.del(amount));
-      return Promise.all(promisesArray);
+
+      return Promise.all(actionsPromises).then(() => {
+        // Ideally the below deletion is done as soon as possible to prevent cases where tallies are finished but deletion
+        // hasn't completed, causing errors in subsequent donation intents
+        return redis.del("donors" + amount, amount);
+      });
     });
-  promiseCareTaker(thePromise);
+  promiseCareTaker(tallyPromise);
 }
 
-//Going to overlook some possible race conditions because it's so rare that someone donates immediately after submitting the
-//donation intent
+// Going to overlook some possible race conditions because it's so rare that someone donates immediately after submitting the
+// donation intent
 app.post("/bank-email", async function (req, res) {
   const { amount } = req.body;
 
@@ -75,12 +77,22 @@ app.post("/bank-email", async function (req, res) {
 });
 
 async function queueIntent(amount, ID, res) {
+  // This function checks the existence of a Redis DB entry then sets the value differently depending on the existence. If this
+  // function is called two times simultaneously, a race condition occurs because executions will read that the entry doesn't exist.
+  // Both functions will execute the logic meant for the non-existent case, which will give a wrong outcome. A
+  // possible solution is to use Redis transactions to read and write atomically, but this is hard because
+  // application logic needs to be interleaved in between the Redis read and write operations. The current solution uses a "mutex"
+  // of sorts "queueIntentMutex", which is a resolved promise. When a request comes in, the promise returned by the queueIntent
+  // function is chained to queueIntentMutex, causing queueIntent to be executed asynchronously. No matter how many requests
+  // come in, their corresponding executions of queueIntent are chained one after the other, ensuring that Redis reads and
+  // writes are never interleaved.
+
   const result = await redis.hexists(amount, "pending");
 
   if (result === 0) {
-    console.log("No result ran");
+    console.log("Donation amount does not exist");
 
-    const execReply = await redis
+    await redis
       .multi()
       .hmset(amount, {
         pending: 1,
@@ -91,10 +103,8 @@ async function queueIntent(amount, ID, res) {
 
     const timeoutId = setTimeout(tallyAmounts, 5 * 60 * 1000, amount);
     timerList[amount] = timeoutId;
-
-    console.log(execReply);
   } else {
-    console.log("yes result ran");
+    console.log("Donation amount exists");
 
     await redis
       .multi()
@@ -105,8 +115,11 @@ async function queueIntent(amount, ID, res) {
     clearTimeout(timerList[amount]);
     timerList[amount] = setTimeout(tallyAmounts, 5 * 60 * 1000, amount);
   }
-  //Response is finished here because I need my test to wait until timeout has been set. Only when timeout has been set can I
-  //jump to the future with Jest's fake timers.
+
+  // Response is finished here because I need my test to wait until timeout has been set. Only when timeout has been set can I
+  // jump to the future with Jest's fake timers.
+  // Doing this means that the mutex can acutally be discarded. We can simply await the queueIntent function within each
+  // request. There will be a performance penalty, though, so try finding a workaround.
   res.send(200);
 }
 
